c74810092c409012a92bd5c139979e4469cca2c6
shivamurthy364 <shiremath043@gmail.com>
2026-06-06 23:41:03 +0530

    Update app

--- main.go
+++ main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello Go!")
+	fmt.Fprintln(w, "Hello Go! This is a simple web server running in my local environment. Okay")
 }
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	http.HandleFunc("/", home)
 
-	fmt.Println("Server running on :8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("Listening on :%s\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
